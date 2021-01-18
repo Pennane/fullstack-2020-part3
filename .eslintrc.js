a6e211a6c08c9fe3b688739a9f2a03631c4721a4
Arttu Pennanen <arttu@pennanen.org>
2021-01-18 13:23:52 +0200

    3.22

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    'no-undef': 0,
+    'no-unused-vars': 0,
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 0
+  }
+}
